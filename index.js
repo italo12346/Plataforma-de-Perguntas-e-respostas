a8e07643fa0616b523bd6639d0def50a5d474b7a
Italo souza sampaio <78382234+italo12346@users.noreply.github.com>
2023-05-07 08:10:14 -0300

    usando body parser para receber dados do formulario

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 
+//Dizendo para o express usar o ejs como view engine
 app.set("view engine",'ejs')
 app.use(express.static('public'))
+//body Parser decodificando os dados da url 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/',(req, res)=>{
     res.render('index')
 })
 app.get('/perguntar',(req,res)=>{
     res.render('perguntar')
+})
+
+app.post('/salvarFormulario',(req ,res)=>{
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send('formulario recebido! '+ titulo +" "+"descriçao "+descricao)
 })
 app.listen(3000,() => console.log("Servidor online"))
